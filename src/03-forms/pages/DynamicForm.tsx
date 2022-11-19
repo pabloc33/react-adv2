adaf2d73b371f008746c4f78b8f415416dff7f6e
pabloc33 <pablo.contino33@gmail.com>
2022-11-18 21:38:17 -0300

    Formularios dinamicos listos

---
+++ src/03-forms/pages/DynamicForm.tsx
@@ -0,0 +1,86 @@
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { MySelect, MyTextInput } from "../components";
+import formJson from "../data/custom-form.json";
+
+const initialValues: { [key: string]: any } = {};
+const requiredFields: { [key: string]: any } = {};
+
+for (const input of formJson) {
+  initialValues[input.name] = input.value;
+
+  if (!input.validations) continue;
+
+  let schema = Yup.string();
+
+  for (const rule of input.validations) {
+    if (rule.type === "required") {
+      schema = schema.required("Este campo es requerido");
+    }
+
+    if (rule.type === "minLength") {
+      schema = schema.min(
+        (rule as any).value || 2,
+        `Mínimo de ${(rule as any).value || 2} caracteres`
+      );
+    }
+
+    if (rule.type === "email") {
+      schema = schema.email("Email no tiene un formato válido");
+    }
+
+    // ... otras reglas
+  }
+
+  requiredFields[input.name] = schema;
+}
+
+const validationSchema = Yup.object({ ...requiredFields });
+
+export const DynamicForm = () => {
+  return (
+    <div>
+      <h1>Dynamic Form</h1>
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(formik) => (
+          <Form>
+            {formJson.map(({ type, label, name, placeholder, options }) => {
+              if (type === "input" || type === "password" || type === "email") {
+                return (
+                  <MyTextInput
+                    key={name}
+                    type={type as any}
+                    label={label}
+                    name={name}
+                    placeholder={placeholder}
+                  />
+                );
+              } else if (type === "select") {
+                return (
+                  <MySelect key={name} label={label} name={name}>
+                    <option value="">Select an option</option>
+                    {options?.map(({ id, description }) => (
+                      <option key={id} value={id}>
+                        {description}
+                      </option>
+                    ))}
+                  </MySelect>
+                );
+              }
+
+              throw new Error(`El Type: ${type}, no es soportado`);
+            })}
+            <button type="submit">Submit</button>{" "}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,10 +6,12 @@ import {
   Navigate,
 } from "react-router-dom";
 import {
+  DynamicForm,
   FormikAbstraction,
   FormikBasicPage,
   FormikComponents,
   FormikYupPage,
+  RegisterFormikPage,
   RegisterPage,
 } from "../03-forms/pages";
 import logo from "../logo.svg";
@@ -65,10 +67,18 @@ export const Navigation = () => {
               </li>
               <li>
                 <NavLink
-                  to="/users"
+                  to="/formik-register"
                   className={({ isActive }) => (isActive ? "nav-active" : "")}
                 >
-                  Users
+                  Register Formik
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dynamic-form"
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  Dynamic Form
                 </NavLink>
               </li>
             </ul>
@@ -80,6 +90,8 @@ export const Navigation = () => {
             <Route path="formik-yup" element={<FormikYupPage />} />
             <Route path="formik-components" element={<FormikComponents />} />
             <Route path="formik-abstraction" element={<FormikAbstraction />} />
+            <Route path="formik-register" element={<RegisterFormikPage />} />
+            <Route path="dynamic-form" element={<DynamicForm />} />
             <Route path="/*" element={<Navigate to="/register" replace />} />
           </Routes>
         </div>
